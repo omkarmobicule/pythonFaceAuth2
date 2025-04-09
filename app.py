88b270d47b8d653ebb4bddf62129caa324627b34
omkar <omkar.shripat@mobicule.com>
2025-04-09 10:19:54 +0530

    Refactor DeepFace call and update app port to 5000
    Removed redundant `model` parameter from DeepFace.verify as it is unnecessary when specifying `model_name`. Changed app port configuration from 8080 to 5000 for standardization or compatibility purposes.

--- app.py
+++ app.py
@@ -75,4 +75,4 @@ def compare_faces():
         return jsonify({"error": str(e)}), 500
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=5000, debug=False)
+    app.run(host="0.0.0.0", port=4000, debug=False)
